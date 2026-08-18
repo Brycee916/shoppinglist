59a26cfe6edcb868f95aeb8ddf6580712642ff6a
brycee916 <brycechao27@gmail.com>
2026-08-17 22:33:38 -0700

    EventService

---
+++ src/main/java/com/example/shoppinglist/service/EventService.java
@@ -0,0 +1,22 @@
+package com.example.shoppinglist.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.example.shoppinglist.model.Event;
+
+public class EventService {
+
+    private Map<Long, Event> events = new HashMap<>();
+
+    public EventService(){}
+    
+    public Event getEventById(long id){
+        Event foundEvent = events.get(id);
+        return foundEvent;
+    }
+    public Event addEvent(Event event){
+        Event newEvent = events.put(event.getId(), event);
+        return events.get(newEvent.getId());
+    }
+}
